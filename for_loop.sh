df442ffc40d0affd32334897cae1d93c48fcddba
Hoangdinh85 <hdinh47056@gmail.com>
2019-05-02 20:04:45 -0700

    Update for_loop.sh

--- for_loop.sh
+++ for_loop.sh
@@ -5,3 +5,4 @@ for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d"
     serverip=$( gcloud compute instances list | grep $servername | awk '{print $4}');
     echo $serverip ;
 done
+gcloud compute ssh hdinh47056@nagios-a --command='sudo systemctl restart nagio'
