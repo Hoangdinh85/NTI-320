ddc45059b6e2133b057f06ff90ad961e3cd053de
Hoangdinh85 <hdinh47056@gmail.com>
2019-05-07 22:07:57 -0700

    Update for_loop_nrpe_install.sh

--- for_loop_nrpe_install.sh
+++ for_loop_nrpe_install.sh
@@ -1,3 +1,1 @@
-for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios-a );  do 
-    gcloud compute ssh --zone us-east4-c hdinh47056@servername --command=sudo 'yum -y install wget&& wget https://raw.githubusercontent.com/Hoangdinh85/NTI-320/master/web-b && sudo bash web-b
-done
+for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios-a );  do gcloud compute ssh --zone us-east4-c hdinh47056@$servername --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/Hoangdinh85/NTI-310/master/nagios-client && sudo bash nagios-client'; done
