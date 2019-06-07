5aa1a2d296a130329394a038f576233eea44ecf5
Hoangdinh85 <hdinh47056@gmail.com>
2019-06-06 18:33:38 -0700

    Update number_of_process.sh

--- number_of_process.sh
+++ number_of_process.sh
@@ -2,19 +2,19 @@
 # This is a script meant to let us test NRPE
 # plugins we write
 
-Status=$(ps aux | grep -c "")                 #the number of count
+Status=$(mount | grep nfs4 | grep -c "")                 #the number of count
 
 status="0"                                  # Change the status to test different alert states
 
-if [ $status -lt "100" ]; then              #-lt = less than
+if [ $status &ge "1" ]; then              #-lt = less than
     echo "STATUS:OK"
     exit 0;
     
-  elif [ $status -ge "150" ]; then
+  elif [ $status &lt "1" ]; then
     echo "STATUS:CRITICAL"
     exit 2;
     
-  elif [ $status -ge "101" ] ; then
+  elif [ $status &lt "1" ] ; then
     echo "STATUS:WARNING"
     exit 1;
     
