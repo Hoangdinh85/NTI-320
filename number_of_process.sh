055cf1810614ac98e7b5343d6d4ff7e99a7b52f0
Hoangdinh85 <hdinh47056@gmail.com>
2019-06-06 18:39:16 -0700

    Update number_of_process.sh

--- number_of_process.sh
+++ number_of_process.sh
@@ -6,18 +6,18 @@ Status=$(mount | grep nfs4 | grep -c "")                 #the number of count
 
 status="0"                                  # Change the status to test different alert states
 
-if [ $status &ge "1" ]; then              #-lt = less than
+if [ "$status" -ge "1" ]; then              #-lt = less than
     echo "STATUS:OK"
     exit 0;
-    
-  elif [ $status &lt "1" ]; then
+
+  elif [ "$status" -lt "1" ]; then
     echo "STATUS:CRITICAL"
     exit 2;
-    
-  elif [ $status &lt "1" ] ; then
+
+  elif [ "$status" -lt "1" ] ; then
     echo "STATUS:WARNING"
     exit 1;
-    
+
 else
    echo "STATUS:UNKNOWN"
    exit 3;
