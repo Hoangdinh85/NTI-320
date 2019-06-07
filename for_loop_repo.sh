19b10c68f5094759aaca9ed23c9ac01b643c8dc6
Hoangdinh85 <hdinh47056@gmail.com>
2019-06-06 20:10:46 -0700

    Create for_loop_repo.sh

---
+++ for_loop_repo.sh
@@ -0,0 +1,1 @@
+for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v repos_srv.sh );  do gcloud compute ssh --zone us-west1-b hdinh47056@$servername --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/Hoangdinh85/NTI-320/master/repos_client.sh && sudo bash repos_client.sh'; done
