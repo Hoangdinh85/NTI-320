f48aabbd97b10ed94087e41bfac3023a1df2dcc8
Hoangdinh85 <hdinh47056@gmail.com>
2019-06-06 18:23:40 -0700

    Update ldapclient.sh

--- ldapclient.sh
+++ ldapclient.sh
@@ -1,20 +1,18 @@
-#automation
 #!/bin/bash
+# based off of https://www.tecmint.com/configure-ldap-client-to-connect-external-authentication
+# with some additions that make it work, as opposed to not work
 
 if [ -e /etc/ldap.secret]; then
-
 exit 0;
-
 fi
+
 apt-get update
 apt-get install -y debconf-utils
 export DEBIAN_FRONTEND=noninteractive
-#this will install and then unset the variable
-apt-get --yes install libnss-ldap libpam-ldap ldap-utils nslcd
+apt-get --yes install libnss-ldap libpam-ldap ldap-utils nscd
 unset DEBIAN_FRONTEND
 
 
-#create /tempfile and add 
 echo "ldap-auth-config ldap-auth-config/bindpw password
 nslcd nslcd/ldap-bindpw password
 ldap-auth-config ldap-auth-config/rootbindpw password
@@ -24,7 +22,7 @@ nslcd nslcd/ldap-starttls boolean false
 libpam-runtime libpam-runtime/profiles multiselect unix, ldap, systemd, capability
 nslcd nslcd/ldap-sasl-authzid string
 ldap-auth-config ldap-auth-config/rootbinddn string cn=ldapadm,dc=nti310,dc=local
-nslcd nslcd/ldap-uris string ldap://ldapc
+nslcd nslcd/ldap-uris string ldap://LDAPC
 nslcd nslcd/ldap-reqcert select
 nslcd nslcd/ldap-sasl-secprops string
 ldap-auth-config ldap-auth-config/ldapns/ldap_version select 3
@@ -39,14 +37,13 @@ ldap-auth-config ldap-auth-config/pam_password select md5
 nslcd nslcd/ldap-sasl-mech select
 nslcd nslcd/ldap-sasl-authcid string
 ldap-auth-config ldap-auth-config/ldapns/base-dn string dc=nti310,dc=local
-ldap-auth-config ldap-auth-config/ldapns/ldap-server string ldap://ldapc/
+ldap-auth-config ldap-auth-config/ldapns/ldap-server string ldap://LDAPC/
 nslcd nslcd/ldap-binddn string
 ldap-auth-config ldap-auth-config/dblogin boolean false" >> tempfile
 
-#moved everything in tempfile to debconf-set-selections
 while read line; do echo "$line" | debconf-set-selections; done < tempfile
 
-echo "P@ssw0rd1" > /etc/ldap.secret
+echo "mypassword" > /etc/ldap.secret
 
 chown 600 /etc/ldap.secret
 
@@ -55,8 +52,10 @@ sudo auth-client-config -t nss -p lac_ldap
 echo "account sufficient pam_succeed_if.so uid = 0 use_uid quiet" >> /etc/pam.d/su
 
 sed -i 's/base dc=example,dc=net/base dc=nti310,dc=local/g' /etc/ldap.conf
-sed -i 's,uri ldapi:///,uri ldap://ldap2/,g' /etc/ldap.conf
+sed -i 's,uri ldapi:///,uri ldap://LDAPC/,g' /etc/ldap.conf
 sed -i 's/rootbinddn cn=manager,dc=example,dc=net/rootbinddn cn=ldapadm,dc=nti310,dc=local/g' /etc/ldap.conf
+
+#for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios4419 );  do gcloud compute ssh --zone us-east1-b koda@$servername --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/dakoda17/NTI-310/master/hello-nti-310/config/nagiosclient421 && sudo bash nagiosclient421'; done
 
 
 systemctl restart nscd
