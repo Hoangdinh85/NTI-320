0dc39536f2655f25e0f4555416bf9f308a893270
Hoangdinh85 <hdinh47056@gmail.com>
2019-06-13 21:01:48 -0700

    Update for_loop_repo.sh

--- for_loop_repo.sh
+++ for_loop_repo.sh
@@ -1,1 +1,2 @@
-for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v repos_srv.sh );  do gcloud compute ssh --zone us-west1-b hdinh47056@$servername --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/Hoangdinh85/NTI-320/master/repos_client.sh && sudo bash repos_client.sh'; done
+#!/bin/bash    
+for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v repos-srv );  do gcloud compute ssh --zone us-west1-b hdinh47056@$servername --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/Hoangdinh85/NTI-320/master/repos_client.sh && sudo bash repos_client.sh'; done
